6e366aaf12c9c9bb8cc48f8637f6adcfa20071b6
Kaut Tanya <kaut.tanya.s@gmail.com>
2024-03-04 17:17:10 +0200

    Add new file

---
+++ main.py
@@ -0,0 +1,15 @@
+"""Logger"""
+# message = input("Enter the message: ")
+
+
+class Logger:
+    """Pass"""
+    def __init__(self, log_level):
+        self.log_level = log_level
+
+    def __call__(self, message):
+        print(f"{self.log_level} : {message}")
+
+
+logger = Logger("Info")
+logger("Something happened!")   # Info: Something happened!
